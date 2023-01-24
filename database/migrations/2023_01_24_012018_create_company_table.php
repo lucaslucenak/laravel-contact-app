0061126b2317fe83fee37988bdd6f41731ca8e00
Lucas Lucena <lucas.lucenak@gmail.com>
2023-01-23 23:40:20 -0300

    Update migrations

--- database/migrations/2023_01_24_012018_create_company_table.php
+++ database/migrations/2023_01_24_012018_create_company_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('companies', function (Blueprint $table) {
+        Schema::create('company', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('address')->nullable();
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('companies');
+        Schema::dropIfExists('company');
     }
 };
